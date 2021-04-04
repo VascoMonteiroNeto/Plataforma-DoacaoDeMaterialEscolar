fc0a779939fb20ac6b018b3d27d99c35a036656f
VascoMonteiroNeto <vascomonteironeto@gmail.com>
2021-04-04 13:34:01 -0300

    Implementação do meunu responsivo

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function showMenu(y) {
+    y.classList.toggle("change");
+    
+    var x = document.getElementById("menuLinks");
+    console.log('clique');
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+} 
